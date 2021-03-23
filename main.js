938b37ec0c41727c8947613c2bdad74ae3fb8fb3
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-03-23 02:12:30 -0300

    lógica de reverificar os inputs e enviar o formulári caso corretamente preenchido

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+
+
 function validateFields(){
     document.getElementById("form").addEventListener("submit", (event)=>{
         event.preventDefault();
+        if(!validateFields()){
+           validateFields()
+           
+       } else{
+        event.target.submit();
+       }
     });
     
     const firstName = document.querySelector(".input-name");
@@ -15,26 +23,46 @@ function validateFields(){
     const password = document.querySelector(".input-password");
     const passwordValue = password.value;
 
+    let status = true;
 
     if(firstNameValue == 0){
         firstName.classList.add("input-error");
         document.querySelector(" .error:nth-child(2) ").style.display = "block";
+        status = false;
 
-    } 
+    } else{
+        document.querySelector(" .error:nth-child(2) ").style.display = "none";
+        firstName.classList.remove("input-error");
+    }
     if(lastNameValue == 0){
         lastName.classList.add("input-error");
         document.querySelector(" .error:nth-child(4) ").style.display = "block";
-    } 
+        status = false;
+    } else{
+        document.querySelector(" .error:nth-child(4) ").style.display = "none";
+        lastName.classList.remove("input-error");
+    }
     
     if(emailValue == 0){
         email.classList.add("input-error");
         document.querySelector(".error:nth-child(6)").style.display = "block";
-    } 
+        status = false;
+    } else{
+        document.querySelector(" .error:nth-child(6) ").style.display = "none";
+        email.classList.remove("input-error");
+    }
+
     if(passwordValue == 0){
         password.classList.add("input-error");
         document.querySelector(".error:nth-child(8)").style.display = "block";
+        status = false;
+    } else{
+        document.querySelector(" .error:nth-child(8) ").style.display = "none";
+        password.classList.remove("input-error");
     }
 
+    return status;
 
 }
+
 
